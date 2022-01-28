587507ac2a985d71e534a6e17e7794b9c11169ab
Jingyu Ham <jingyu.ham94@gmail.com>
2022-01-29 01:23:01 +0900

    complete "Next Big Number" test

--- programmers/L2P58_NextBigNumber.java
+++ programmers/L2P58_NextBigNumber.java
@@ -4,10 +4,13 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        int count = countOne(n);
+        // bitCount메소드는 Integer형 숫자의 비트데이터에서 1의 개수를 return하는 메소드입니다.
+        int count = Integer.bitCount(n);
+        
+        // n을 1씩 증가시키며 1의 개수가 같은 n을 찾습니다.
         while(true) {
             n++;
-            if(count == countOne(n)) {
+            if(count == Integer.bitCount(n)) {
                 break;
             }
         }
@@ -16,43 +19,4 @@ class Solution {
         
         return answer;
     }
-    
-    
-    public int countOne(int i) {
-        int count = 0;
-        String s = Integer.toBinaryString(i).replaceAll("0", "");
-        
-        return s.length();
-    }
 }
-
-
-/*
-1   2   4   8
-1   10  100 1000
-
-3   5   6   9
-11  101 110 1001
-
-
-7     11    13      14
-111   1011  1101    1110
-
-
-
-1001110     끝이 0일때, 왼쪽이 0인 1을 찾음, 왼쪽을 1로 바꾸고 남은 1들을 끝에서부터 채움
-1010011
-
-11100       끝이 0일때, 왼쪽이 0인 1을 못찾음, 0의 갯수+1, 끝에서부터 남은 1들을 채움
-100011
-
-1011100
-1100011
-
-1010101     끝이 1일때, 왼쪽이 0인 1을 찾음, 왼쪽을 1로 바꾸고 현재위치를 0으로바꿈
-1010110
-
-1111        끝이 1일때, 왼쪽이 0인 1을 못찾음, 0의 갯수+1, 끝에서부터 남은 1들을 채움
-10111
-
-*/
