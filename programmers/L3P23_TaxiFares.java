d28e3ade56082fe12ad45c5590371f41aad21437
Jingyu Ham <jingyu.ham94@gmail.com>
2022-03-11 21:44:18 +0900

    add "Taxi Fares" test -incomplete

---
+++ programmers/L3P23_TaxiFares.java
@@ -0,0 +1,29 @@
+// 합승 택시 요금
+// https://programmers.co.kr/learn/courses/30/lessons/72413?language=java
+
+class Solution {
+    public int solution(int n, int s, int a, int b, int[][] fares) {
+        int answer = 0;
+        
+        int[][] route = new int[n][n];
+        boolean[] visited = new boolean[n];
+        int[] distance = new int[n];
+        
+        return answer;
+    }
+    
+    
+    
+    public void dijkstra(int n, int s, int[] distance, boolean[] visited, int[][] fares) {
+        
+    }
+    
+}
+
+
+/*
+시작지점 s부터 합승구간을 k지점까지 감 (k는 시작 포함 모든 지점이 가능)
+k지점에서 각자 도착지점까지 최소 거리를 더함 
+
+
+*/
