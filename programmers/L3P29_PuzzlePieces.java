630c2d6e6a40db6c3cb390c53d5fad855768aced
Jingyu Ham <jingyu.ham94@gmail.com>
2022-03-18 22:53:29 +0900

    add "Fill Puzzle Pieces" test -incomplete

---
+++ programmers/L3P29_PuzzlePieces.java
@@ -0,0 +1,16 @@
+// 퍼즐 조각 채우기
+// https://programmers.co.kr/learn/courses/30/lessons/84021?language=java
+
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
+        int answer = 0;
+        
+        return answer;
+    }
+    
+    public int bfs() {
+
+    }
+}
