60ba6ce905bcfdcc1f427cb1e00660359dc752e0
Jingyu Ham <jingyu.ham94@gmail.com>
2022-03-25 23:19:10 +0900

    modify "Ad Insertion" test

--- programmers/L3P30_AdInsertion.java
+++ programmers/L3P30_AdInsertion.java
@@ -1,99 +1,91 @@
-// 퍼즐 조각 채우기
-// https://programmers.co.kr/learn/courses/30/lessons/84021?language=java
-
-import java.util.*;
+// 광고 삽입
+// https://programmers.co.kr/learn/courses/30/lessons/72414?language=java
 
 class Solution {
-    public int solution(int[][] game_board, int[][] table) {
-        int answer = -1;
-        int n = game_board.length;
-        boolean[][] visited_game_board = new boolean[n][n];
-        boolean[][] visited_table = new boolean[n][n];
-        List<List<int[]>> game_board_pieces = new ArrayList<>();
-        List<List<int[]>> table_pieces = new ArrayList<>();
+    public String solution(String play_time, String adv_time, String[] logs) {
+        String answer = "";
+        // 동영상의 최대 시간은 99:59:59로 초로 환산하면 359999초입니다.
+        // 초단위로 누적 시청 시간을 계산하므로 360000크기의 배열을 사용합니다.
+        int[] s_time = new int[360000];
         
-        for(int r = 0 ; r < n ; r++) {
-            for(int c = 0 ; c < n ; c++) {
-                if(game_board[r][c] == 0 && !visited_game_board[r][c]) {
-                    bfs(r, c, game_board, visited_game_board, game_board_pieces, 0);
-                }
+        // 시청자들이 재생한 구간에 해당하는 초의 값을 증가시킵니다.(해당 구간을 재생할 경우 초값만큼 누적 재생시간이 증가합니다.)
+        for(String log : logs) {
+            String[] l_arr = log.split("-");
+            int start = timeToSeconds(l_arr[0]);
+            int end = timeToSeconds(l_arr[1]);
+            
+            for(int i = start ; i < end ; i++) {
+                s_time[i]++;
             }
         }
         
-        // System.out.println("test");
-        // int t1 = 5;
-        // int t2 = 5;
-        // System.out.println(5>5);
-        // HashSet<int[]> ts = new HashSet<>();
-        // HashSet<int[]> ts2 = new HashSet<>();
-        // int[] a = {1};
-        // int[] b = {1};
-        // ts.add(a);
-        // ts2.add(b);
-        // System.out.println(ts.contains(a));
-        // System.out.println(ts.contains(b));
+        // max_time_sum : 최대 누적 시간, time_sum : 누적 시간, p_sec : 동영상 재생시간, a_sec : 광고 재생시간, adv_start_time : 광고 시작 시각
+        long max_time_sum = 0;
+        long time_sum = 0;
+        int p_sec = timeToSeconds(play_time);
+        int a_sec = timeToSeconds(adv_time);
+        int adv_start_time = 0;
+        
+        // 초기값으로 동영상의 시작 시각부터 광고 재생시간만큼 누적 시간을 계산합니다.
+        for(int i = 0 ; i < a_sec ; i++) {
+            time_sum += s_time[i];
+        }
+        max_time_sum = time_sum;
+        
+        // 광고 종료시각부터 1초씩 광고 재생구간을 변경하며 누적 시간을 계산합니다.
+        for(int i = a_sec ; i < p_sec ; i++) {
+            // 다음 1초의 누적 시간을 더하고, 광고 시작 시각 1초의 누적 시간을 뺍니다.
+            time_sum += s_time[i];
+            time_sum -= s_time[i-a_sec];
+            if(time_sum > max_time_sum) {
+                max_time_sum = time_sum;
+                // i는 광고 종료 시각이므로 i에서 재생 시간을 빼야 시작 시각이 됩니다.
+                // 1부터 시작하는게 아닌 0부터 시작하는 것이므로 1을 더해줍니다.
+                adv_start_time = i-a_sec+1;
+            }
+        }
+        
+        
+        answer = secondsToTime(adv_start_time);
         
         return answer;
     }
     
-    // 주어진 좌표(r, c)로 부터 시작되는 조각의 모든 좌표를 찾는 함수입니다.
-    public void bfs(int r, int c, int[][] board, boolean[][] visited, List<List<int[]>> pieces, int condition) {
-        // 상, 하, 좌, 우로 이동하는 가중치입니다.
-        int[][] nextVal = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        Queue<int[]> q = new LinkedList<>();
-        // 조각을 이루는 칸의 모든 좌표를 저장할 배열리스트입니다.
-        List<int[]> piece = new ArrayList<>();
-        visited[r][c] = true;
-        q.offer(new int[] {r, c});
+    
+    // HH:MM:SS 형태의 시간을 초 형태로 변환하는 함수입니다.
+    public int timeToSeconds(String t) {
+        int seconds = 0;
+        String[] t_arr = t.split(":");
+        seconds += Integer.parseInt(t_arr[0])*3600;
+        seconds += Integer.parseInt(t_arr[1])*60;
+        seconds += Integer.parseInt(t_arr[2]);
         
-        while(!q.isEmpty()) {
-            int[] p = q.poll();
-            int pr = p[0];
-            int pc = p[1];
-            // p[0] -= r;
-            // p[1] -= c;
-            // puzzle_coordinates.add(p);
-            // 조각을 이루는 칸의 좌표값을 저장합니다. (단, 좌표값은 (0, 0)을 기준으로 합니다.)
-            piece.add(new int[] {p[0]-r, p[1]-c});
-            // 상, 하, 좌, 우로 이동합니다.
-            for(int i = 0 ; i < 4 ; i++) {
-                int nr = pr + nextVal[i][0];
-                int nc = pc + nextVal[i][1];
-                // 새로운 좌표가 보드의 범위를 벗어나면 제외합니다.
-                if(nr < 0 || nr >= board.length || nc < 0 || nc >= board.length) {
-                    continue;
-                }
-                // 새로운 좌표가 조건(condition, game_board 에서는 0, table 에서는 1)에 맞으며
-                // 방문한 좌표가 아닐 경우 큐에 넣습니다.
-                if(board[nr][nc] == condition && !visited[nr][nc]) {
-                    visited[nr][nc] = true;
-                    q.offer(new int[] {nr, nc});
-                }
-            }
-        }
-        
-        // 반복문이 종료되면 조각의 모든 좌표를 저장하였으므로 조각(piece)를 조각들(pieces)에 넣습니다.
-        pieces.add(piece);
-        // System.out.println("!");
-        // for(int[] co : puzzle_coordinates) {
-        //     System.out.println(co[0]+" "+co[1]);
-        // }
-        rotatePiece(piece);
+        return seconds;
     }
     
-    public List<int[]> rotatePiece(List<int[]> piece) {
-        for(int[] a : piece) {
-            System.out.println(a[0]+"/"+a[1]);
+    // 초 형태의 시간을 HH:MM:SS 형태로 변환하는 함수입니다.
+    public String secondsToTime(int t) {
+        String hours = Integer.toString(t/3600);
+        t %= 3600;
+        if(hours.length() < 2) {
+            hours = "0"+hours;
         }
-        Collections.sort(piece, (o1, o2) -> o1.compareTo(o2));
-        for(int[] a : piece) {
-            System.out.println(a[0]+"/"+a[1]);
+        String minutes = Integer.toString(t/60);
+        t %= 60;
+        if(minutes.length() < 2) {
+            minutes = "0"+minutes;
         }
-        // for(int i = 0 ; i < piece.length ; i++) {
-        //     piece.get(i)
-        // }
-        
-        return piece;
+        String seconds = Integer.toString(t);
+        if(seconds.length() < 2) {
+            seconds = "0"+seconds;
+        }
+        return hours+":"+minutes+":"+seconds;
     }
-    
 }
+
+/*
+초단위로 환산
+99시간 59분 59초 = 100시간 미만
+100시간 = 360000초
+359999까지
+*/
