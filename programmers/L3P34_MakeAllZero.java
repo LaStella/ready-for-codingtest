6e6182f694fd7fd14503892b640ebbad5a72ff1c
Jingyu Ham <jingyu.ham94@gmail.com>
2022-03-27 02:24:50 +0900

    modify "Make All Zero" test -complete

--- programmers/L3P34_MakeAllZero.java
+++ programmers/L3P34_MakeAllZero.java
@@ -5,81 +5,70 @@ import java.util.*;
 
 class Solution {
     long answer = 0;
+    long[] global_a;
+    List<Integer>[] edge_list;
+    boolean[] visited;
+    
     public long solution(int[] a, int[][] edges) {
-        boolean[][] exist_edges = new boolean[a.length][a.length];
-        ArrayList<Node> node_list = new ArrayList<>();
-        boolean[] visited = new boolean[a.length];
+        // 각 정점의 가중치를 long타입으로 저장할 배열입니다.
+        global_a = new long[a.length];
+        // 간선을 저장할 리스트입니다.
+        edge_list = new ArrayList[a.length];
+        // 정점의 방문 여부를 저장할 배열입니다.
+        visited = new boolean[a.length];
         
-        for(int[] edge : edges) {
-            exist_edges[edge[0]][edge[1]] = true;
-            exist_edges[edge[1]][edge[0]] = true;
+        long sum = 0;
+        
+        for(int i = 0 ; i < a.length ; i++) {
+            sum += a[i];
+            global_a[i] = a[i];
+            edge_list[i] = new ArrayList<Integer>();
         }
         
-        int max_index = 0;
-        int max_value = 0;
+        // 모든 정점의 가중치합이 0이 아닐 경우 모든 정점을 0으로 만드는 것이 불가능합니다.
+        if(sum != 0) return answer = -1;
         
-        for(int i  = 0 ; i < a.length ; i++) {
-            node_list.add(new Node(i, a[i]));
-            if(Math.abs(a[i]) > max_value) {
-                max_index = i;
-                max_value = Math.abs(a[i]);
-            }
+        // 간선을 양방향으로 저장합니다.
+        // boolean[][] 배열을 통해 저장할 수 있으나, 정점이 많아 메모리 크기를 초과할 수 있습니다.
+        for(int i = 0 ; i < edges.length ; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            edge_list[u].add(v);
+            edge_list[v].add(u);
         }
         
-        
-        
-        Queue<Node> q = new LinkedList<>();
-        q.add(node_list.get(max_index));
-        
-        while(!q.isEmpty()) {
-            Node p_node = q.poll();
-            visited[p_node.index] = true;
-            for(int i = 0 ; i < a.length ; i++) {
-                if(exist_edges[p_node.index][i] && !visited[i]) {
-                    q.add(node_list.get(i));
-                    p_node.child_list.add(node_list.get(i));
-                }
-            }
-        }
-        
-        if(postorder(node_list.get(max_index)) != 0) {
-            answer = -1;
-        }
+        // 0번 정점을 head로 하는 트리입니다.
+        // 한 정점을 기준으로 트리를 만들어 자식에서 부모로 가중치를 더해가므로 어느 정점을 head로 하든 상관없습니다.
+        dfs(0);
         
         return answer;
     }
     
-    public int postorder(Node head) {
-        if(head.child_list.size() != 0) {
-            for(Node child : head.child_list) {
-                int child_weight = postorder(child);
-                answer += Math.abs(child_weight);
-                head.weight += child_weight;
+    // 주어진 index번 정점의 가중치를 리턴하는 함수입니다.
+    // index의 가중치는 index의 모든 자식 정점의 가중치에 자신의 가중치까지 더해야합니다.
+    public long dfs(int index) {
+        // 정점을 방문처리합니다.
+        visited[index] = true;
+        
+        // 정점과 이어진 다른 정점들을 찾습니다.
+        for(int i = 0 ; i < edge_list[index].size() ; i++) {
+            int child_index = edge_list[index].get(i);
+            // 방문하지 않은 정점이라면 index정점의 자식이 됩니다.
+            // 따라서 자식 정점의 가중치를 더하여야 합니다.
+            if(!visited[child_index]) {
+                long child_value = dfs(child_index);
+                global_a[index] += child_value;
+                // 자식 정점의 가중치를 더한만큼 행동 횟수가 증가합니다.
+                answer += Math.abs(child_value);
             }
         }
         
-        return head.weight;
+        return global_a[index];
     }
 }
-
-class Node {
-    int index;
-    int weight;
-    ArrayList<Node> child_list;
-    
-    public Node(int index, int weight) {
-        this.index = index;
-        this.weight = weight;
-        child_list = new ArrayList<>();
-    }
-}
-
 
 
 /*
 트리 형성
 자식들부터 차례로 순회 
-헤드는 절대값이 가장큰것
-
-
 */
