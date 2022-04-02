e7a3e31ad28b16b4eabaceb420df3a2d6caac4e8
Jingyu Ham <jingyu.ham94@gmail.com>
2022-04-03 00:01:14 +0900

    modify "Exterior Wall Check" test -complete

--- programmers/L3P37_ExteriorWallCheck.java
+++ programmers/L3P37_ExteriorWallCheck.java
@@ -4,26 +4,33 @@
 import java.util.*;
 
 class Solution {
+    int answer;
     public int solution(int n, int[] weak, int[] dist) {
-        int answer = 0;
-        int[] ex_weak = new int[weak.length*2];
-        
-        for(int i = 0 ; i < ex_weak.length ; i++) {
-            ex_weak[i] = weak[i%weak.length]+n*(i/weak.length);
-        }
-        
+        // 초기값으로 최대인원보다 큰 수를 저장합니다.
+        answer = dist.length+1;
+        int[][] ex_weak = new int[weak.length][weak.length];
         boolean[] visited = new boolean[dist.length];
+        
+        makeExtendedWeak(weak, n, ex_weak);
         dfs(0, new int[dist.length], n, ex_weak, dist, visited);
         
-        System.out.println(Arrays.toString(ex_weak));
+        // 모든 인원을 투입해도 취약 지점을 전부 점검할 수 없는 경우 -1을 리턴합니다.
+        if(answer == dist.length+1) {
+            return -1;
+        }
         
         return answer;
     }
     
-    public void dfs(int depth, int[] result, int n, int[] ex_weak, int[] dist, boolean[] visited) {
+    // 투입할 친구의 순열을 구하며, 구한 순열에 대해 취약점을 점검하는 함수입니다.
+    // depth : 깊이, result : 순열이 저장되는 공간
+    public void dfs(int depth, int[] result, int n, int[][] ex_weak, int[] dist, boolean[] visited) {
+        // 순열이 구하면 취약점을 점검합니다.
         if(depth == dist.length) {
-            System.out.println(Arrays.toString(result));
-            check(result, n, ex_weak);
+            // 취약점을 순차방문하여 모든 취약점을 방문합니다.
+            for(int[] weak : ex_weak) {
+                check(result, n, weak);    
+            }
         }
         else {
             for(int i = 0 ; i < dist.length ; i++) {
@@ -37,9 +44,10 @@ class Solution {
         }
     }
     
-    public void check(int[] result, int n, int[] ex_weak) {
-        // 취약점 시작 지점입니다.
-        int s_index = 0;
+    // 취약점을 점검하는 함수입니다.
+    public void check(int[] result, int n, int[] weak) {
+        // 취약점을 가리키는 커서입니다.
+        int cursor = 0;
         // 점검을 보낸 사람의 수입니다.
         int count = 0;
         // 점검이 확인된 지점을 저장하는 set입니다.
@@ -47,27 +55,40 @@ class Solution {
         
         for(int d : result) {
             // 보낸 사람이 점검을 시작해서 종료하는 지점입니다.
-            int e_point = ex_weak[s_index] + d;
+            int end = weak[cursor] + d;
             // 외벽의 취약점을 순회하며 종료지점안에 포함되는 취약점은 점검합니다.
-            for(int i = s_index ; i < ex_weak.length ; i++) {
-                if(ex_weak[i] <= e_point) {
-                    System.out.print("!");
-                    set.add(ex_weak[i]%n);
-                    s_index++;
+            for(int i = cursor ; i < weak.length ; i++) {
+                // 취약점을 점검하면 다음 취약점으로 커서를 이동합니다.
+                if(weak[i] <= end) {
+                    set.add(weak[i]%n);
+                    cursor++;
                 }
                 // 종료지점을 벗어나는 지점은 점검할 수 없으므로 점검을 종료합니다.
                 else {
                     break;
                 }
             }
-            System.out.println();
             // 점검을 종료하면 점검한 사람의 수를 증가시킵니다.
             count++;
             // 점검을 모두 했다면 중단합니다.
-            if(set.size() == ex_weak.length/2) {
-                System.out.println(count);
+            if(set.size() == weak.length) {
+                answer = Math.min(answer, count);
                 break;
             }
+        }
+    }
+    
+    // 취약점 배열의 순서를 하나씩 바꾸어 저장하는 함수입니다.
+    public void makeExtendedWeak(int[] weak, int n, int[][] ex_weak) {
+        // ex_weak[i]는 i-1번째 취약점이 가장 마지막에 배치되는 경우입니다. (단, i >= 1)
+        ex_weak[0] = weak.clone();
+        for(int i = 1 ; i < ex_weak.length ; i++) {
+            int temp = weak[0];
+            for(int j = 0 ; j < weak.length-1 ; j++) {
+                weak[j] = weak[j+1];
+            }
+            weak[weak.length-1] = temp+n;
+            ex_weak[i] = weak.clone();
         }
     }
 }
@@ -80,6 +101,8 @@ ex) a = 1, b = 10 , n = 12
 13-10 = 3
 1 = 1+n
 weak배열을 확장 [1,2,3,..,i] -> [1,2,3,..,1+n,2+n,...i+n]
+[1,2,3,4] ,[2,3,4,13], [3,4,13,14], [4,13,14,15]
+n개의 원소를 가지는 weak배열은 n개의 한방향으로 순서만 뒤바꾼 배열로 바꿀 수 있다.
 한방향으로 계산이 가능
 
 dist의 원소의 순열에 따라 순차적으로 진행
