1d2cd724325d5e92ef223ac11d63cfbf9322f8f2
Jingyu Ham <jingyu.ham94@gmail.com>
2022-04-13 23:35:42 +0900

    modify "Move Between Blocks" test -incomplete

--- programmers/L3P41_MoveBetweenBlocks.java
+++ programmers/L3P41_MoveBetweenBlocks.java
@@ -9,10 +9,9 @@ class Solution {
         int n = board.length-1;
         
         int[] end_point = {n, n};
-        Queue<int[][]> q = new LinkedList<>();
+        Queue<Robot> q = new LinkedList<>();
         boolean[][] visited = new boolean[n+1][n+1];
-        q.add(new int[][] {{0, 0}, {0, 1}});
-        new Robot(0, new int[][] {{0, 0}, {0, 1}});
+        q.add(new Robot(0, new int[][] {{0, 0}, {0, 1}}));
         
         // int[][] a = new int[][] {{4, 5}, {5, 5}};
         // int[][] b = new int[][] {{4, 5}, {5, 5}};
@@ -20,9 +19,9 @@ class Solution {
         // System.out.println(Arrays.equals(a[0], new int[] {4, 5}));
         
         while(!q.isEmpty()) {
-            int[][] location = q.poll();
+            Robot p_robot = q.poll();
             
-            if(Arrays.equals(location[0], end_point) || Arrays.equals(location[1], end_point)) {
+            if(Arrays.equals(p_robot.l[0], end_point) || Arrays.equals(p_robot.l[1], end_point)) {
                 
             }
         }
