dee20dee18fbeb46ae477fb1a470c1294b9ca165
Jingyu Ham <jingyu.ham94@gmail.com>
2022-04-13 23:57:42 +0900

    modify "Move Between Blocks" test -incomplete (add u/d/l/r move)

--- programmers/L3P41_MoveBetweenBlocks.java
+++ programmers/L3P41_MoveBetweenBlocks.java
@@ -4,15 +4,20 @@
 import java.util.*;
 
 class Solution {
+    int n;
     public int solution(int[][] board) {
         int answer = 0;
-        int n = board.length-1;
+        n = board.length;
         
-        int[] end_point = {n, n};
+        int[] end_point = {n-1, n-1};
+        int[][] next_value = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
         Queue<Robot> q = new LinkedList<>();
-        boolean[][] visited = new boolean[n+1][n+1];
-        q.add(new Robot(0, new int[][] {{0, 0}, {0, 1}}));
+        boolean[][] visited = new boolean[n][n];
         
+        // 시작지점 (0, 0), (0, 1)로부터 시작합니다.
+        q.add(new Robot(0, new int[][] {{0, 0}, {0, 1}}));
+        visited[0][0] = true;
+        visited[0][1] = true;
         // int[][] a = new int[][] {{4, 5}, {5, 5}};
         // int[][] b = new int[][] {{4, 5}, {5, 5}};
         // int[] t = {4, 5};
@@ -22,7 +27,21 @@ class Solution {
             Robot p_robot = q.poll();
             
             if(Arrays.equals(p_robot.l[0], end_point) || Arrays.equals(p_robot.l[1], end_point)) {
-                
+                System.out.println(p_robot.t);
+                break;
+            }
+            else {
+                for(int i = 0 ; i < 4 ; i++) {
+                    int[][] nl = new int[2][2];
+                    nl[0][0] = p_robot.l[0][0]+next_value[i][0];
+                    nl[0][1] = p_robot.l[0][1]+next_value[i][1];
+                    nl[1][0] = p_robot.l[1][0]+next_value[i][0];
+                    nl[1][1] = p_robot.l[1][1]+next_value[i][1];
+                    if(isOutOfBoard(nl[0][0]) || isOutOfBoard(nl[0][1]) || isOutOfBoard(nl[1][0]) || isOutOfBoard(nl[1][1])) {
+                        continue;
+                    }
+                    q.add(new Robot(p_robot.t+1, nl));
+                }
             }
         }
         
@@ -31,13 +50,18 @@ class Solution {
         
         return answer;
     }
+    
+    public boolean isOutOfBoard(int l) {
+        if(l < 0 || l >= n) return true;
+        else return false;
+    }
 }
 
 class Robot {
-    int d;
+    int t;
     int[][] l;
-    Robot(int d, int[][] l) {
-        this.d = d;
+    Robot(int t, int[][] l) {
+        this.t = t;
         this.l = l;
     }
 }
