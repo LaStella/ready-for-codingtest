cce4848a81a2b5644c67c14f032953ec7de3362e
Jingyu Ham <jingyu.ham94@gmail.com>
2022-04-25 00:06:03 +0900

    modify "Number Game" test -complete(faster)

--- programmers/L3P47_NumberGame.java
+++ programmers/L3P47_NumberGame.java
@@ -6,22 +6,25 @@ import java.util.*;
 class Solution {
     public int solution(int[] A, int[] B) {
         int answer = 0;
-        A = Arrays.stream(A).boxed().sorted(Collections.reverseOrder()).mapToInt(i->i).toArray();
-        B = Arrays.stream(B).boxed().sorted(Collections.reverseOrder()).mapToInt(i->i).toArray();
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
         
         int a_index = 0;
         int b_index = 0;
 
-        // A와 B를 내림차순 정렬한 후 B값이 큰 경우 점수를 더합니다.
-        // B값이 작은 경우 A의 다음 숫자와 비교합니다.
-        while(a_index < A.length) {
+        // 오름차순 정렬된 두 배열을 비교합니다.
+        // B의 값이 큰 경우 승점을 추가하며, 두 배열을 가리키는 index를 하나씩 늘립니다.
+        // B의 값이 작거나 같다면, 더 큰 값을 비교하도록 배열B를 가리키는 index만 늘립니다.
+        // 모든 배열B의 원소를 사용하였다면 더 이상 승점을 얻을 수 없으므로 종료합니다.
+        while(b_index < B.length) {
             if(A[a_index] < B[b_index]) {
                 a_index++;
                 b_index++;
                 answer++;
             }
             else {
-                a_index++;
+                b_index++;
             }
         }
         
