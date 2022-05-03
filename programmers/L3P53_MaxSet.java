276e5b1c46cf3bb933686e9c000980151e8b9a21
Jingyu Ham <jingyu.ham94@gmail.com>
2022-05-03 23:30:35 +0900

    modify "Max Set" test -complete

--- programmers/L3P53_MaxSet.java
+++ programmers/L3P53_MaxSet.java
@@ -1,9 +1,32 @@
 // 최고의 집합
 // https://programmers.co.kr/learn/courses/30/lessons/12938?language=java
 
+import java.util.*;
+
 class Solution {
     public int[] solution(int n, int s) {
         int[] answer = {};
+        
+        // 자연수의 개수 n이 합 s보다 크다면 1보다 작은 자연수는 존재하지 않으므로 최고의 집합이 존재할 수 없습니다.
+        if(n > s) {
+            answer = new int[] {-1};
+            return answer;
+        }
+        // 자연수의 합 s를 n으로 나눈 값을 배열에 채웁니다.
+        // 나머지는 배열의 끝에서부터 하나씩 채워줍니다.
+        else {
+            answer = new int[n];
+            Arrays.fill(answer, s/n);
+            if(s%n != 0) {
+                int remain = s%n;
+                int index = n-1;
+                while(remain > 0) {
+                    answer[index--] += 1;
+                    remain--;
+                }
+                
+            }
+        }
         
         return answer;
     }
@@ -30,6 +53,11 @@ d[2][3] = 2 = d[1][1]*(3-1) d[1][2]*(3-2)
 d[2][4] = d[1][1]*(4-1) d[1][2]*(4-2) d[1][3]*(4-3)
 d[3][3] = 1
 d[3][6] = d[2][2]*(6-2)  d[2][3]*(6-3)  d[2][4]*(6-4)  d[2][5]*(6-5)
+--------------------------------------------------------------------------
+n개의 자연수 합 s
+
+s를 n으로 나눈 값으로만 이루어진 집합
+{s/n, s/n, s/n ...}
 
 
 
