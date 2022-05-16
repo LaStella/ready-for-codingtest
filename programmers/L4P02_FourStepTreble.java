5bf125476205ab5ae402344425d8996a125fbbd3
Jingyu Ham <jingyu.ham94@gmail.com>
2022-05-16 23:28:04 +0900

    add "Four Step Treble" test -incomplete

---
+++ programmers/L4P02_FourStepTreble.java
@@ -0,0 +1,27 @@
+// 4단 고음
+// https://programmers.co.kr/learn/courses/30/lessons/1831?language=java
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        return answer;
+    }
+}
+
+/*
+
+*++
+
+*++(*++) (3x+2)3+2 = 9x+8
+*+(*++)+ (3x+1)3+3 = 9x+6
+*(*++)++ (3x)3+4 = 9x+4
+
+*++h
+*+h+
+*h++
+
+n=41
+5(h) = 41 x
+4(h)+1 = 41 -> h = 10 o
+3(h)+2 = 41 -> h = 13 o
+*/
