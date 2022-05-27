f858e2858a216fb5be7e57b3f6f6101734e0f7dc
Jingyu Ham <jingyu.ham94@gmail.com>
2022-05-27 23:38:29 +0900

    modify "Word Puzzle" test -incomplete(using combination)

--- programmers/L4P05_WordPuzzle.java
+++ programmers/L4P05_WordPuzzle.java
@@ -4,45 +4,70 @@
 import java.util.*;
 
 class Solution {
+    int answer;
+    int length;
     public int solution(String[] strs, String t) {
-        int answer = 0;
+        // 초기값은 제한사항에 따라 결과로 나올 수 없는 101로 합니다. (단어 조각은 최대 100개이므로)
+        answer = 101;
+        length = t.length();
         HashSet<String> set = new HashSet<>();
+        List<String> list = new ArrayList<>();
         
         for(String str : strs) {
+            list.add(str);
             set.add(str);
         }
         
-        answer = getMin(t, set);
+        Collections.sort(list, (o1, o2) -> o2.length()-o1.length());
         
-        return answer;
+        System.out.println(list);
+        
+        // String test = "nabnabn";
+        // test = test.replaceAll("n", "");
+        // System.out.println(test);
+        // test = test.replaceAll("abab", "");
+        // System.out.println(test);
+        // getMin(0, t, set);
+        dfs(0, 0, t ,list);
+        
+        // 초기값 그대로라면 주어진 단어를 조각들을 이용하여 문장을 완성할 수 없음을 의미합니다.
+        return answer == 101 ? -1 : answer;
     }
-    
+    public void dfs(int result, int depth, String t, List<String> list) {
+        System.out.println(t);
+        if(t.equals(" ".repeat(length))) {
+            System.out.println(result);
+            answer = Math.min(answer, result);
+        }
+        else if(depth == list.size()) {
+            
+        }
+        else {
+            String str = list.get(depth);
+            if(t.contains(str)) {
+                String next_t = t.replaceAll(str, " ".repeat(str.length()));
+                int count = (t.length()-t.replaceAll(str, "").length())/str.length();
+                dfs(result+count, depth+1, next_t, list);
+            }
+            dfs(result, depth+1, t, list);
+        }
+    }
     // 주어진 단어t를 두 단어로 쪼개어 사용하는 단어 조각 개수의 최소값을 구하는 함수입니다.
-    public int getMin(String t, HashSet<String> set) {
+    public void getMin(int result, String t, HashSet<String> set) {
         // 빈 단어인 경우 단어 조각을 찾을 필요가 없으므로 0을 반환합니다.
         if(t.equals("")) {
-            return 0;
+            answer = Math.min(answer, result);
         }
-        // 초기값은 제한사항에 따라 결과로 나올 수 없는 101로 합니다. (단어 조각은 최대 100개이므로)
-        int result = 101;
         
         // 주어진 단어 t를 최소 1개부터 t의 길이 만큼까지 두 단어로 분리합니다. (ex. t="abc", f_word = "abc", b_word = "")
-        for(int i = 1 ; i <= t.length(); i++) {
+        for(int i = 1 ; i <= t.length() ; i++) {
             String f_word = t.substring(0, i);
             // 분리한 앞단어의 단어 조각이 존재하는 경우에만 뒷단어의 조각 개수를 구합니다.
             if(set.contains(f_word)) {
                 String b_word = t.substring(i, t.length());
-                int b_word_result = getMin(b_word, set);
-                // 뒷단어의 단어 조각이 존재하지 않으면 넘어갑니다.
-                if(b_word_result == -1) continue;
-                
-                // 분리한 두 단어의 총 조각 개수를 비교하여 최소값을 저장합니다.
-                result = Math.min(result, 1+b_word_result);
+                getMin(result+1, b_word, set);
             }
-        }    
-        
-        // result가 초기값 그대로라면 주어진 단어 t를 두 단어로 분리하지 못하였으므로 -1을 반환합니다.
-        return result == 101 ? -1 : result;
+        }
     }
 }
 
@@ -52,4 +77,15 @@ class Solution {
 front 0~mid
 back mid+1~end
 front 단어 조각에 들어있으면 분리 가능하며 없다면 분리 불가능
+시간초과
+-----------------------------------------
+단어 조각 목록을 기준으로 조합을 구함
+단어 조각이 긴것부터 사용하는 것이 유리
+["app","ap","p","l","e","ple","pp"]
+apple
+app,l,e보다 ap,ple가 적은 조각을 사용
+app를 선택한 경우와 선택하지 않은 경우
+ple를 선택한 경우와 선택하지 않은 경우 ...
+
+
 */
