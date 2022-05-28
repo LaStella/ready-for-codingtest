2243dc5ba0e4081a4bf63c6d4a258fc1b8f9a2f0
Jingyu Ham <jingyu.ham94@gmail.com>
2022-05-28 23:59:31 +0900

    modify "Word Puzzle" test -incomplete(using dynamic programming)

--- programmers/L4P05_WordPuzzle.java
+++ programmers/L4P05_WordPuzzle.java
@@ -4,74 +4,43 @@
 import java.util.*;
 
 class Solution {
-    int answer;
-    int length;
     public int solution(String[] strs, String t) {
         // 초기값은 제한사항에 따라 결과로 나올 수 없는 101로 합니다. (단어 조각은 최대 100개이므로)
-        answer = 101;
-        length = t.length();
-        HashSet<String> set = new HashSet<>();
-        List<String> list = new ArrayList<>();
+        int answer = 101;
         
-        for(String str : strs) {
-            list.add(str);
-            set.add(str);
+        int[] dp = new int[20000];
+        
+        // t의 첫번째 문자부터 최소 조각 수를 계산합니다.
+        for(int i = 0 ; i < t.length() ; i++) {
+            // i번째에 해당하는 문자를 가져옵니다.
+            char c = t.charAt(i);
+            
+            for(int j = 0 ; j < strs.length ; j++) {
+                // 조각의 마지막 문자가 c와 같으면 조각을 넣을 수 있습니다.
+                // 
+                if(strs[j].charAt(strs[j].length()-1) == c) {
+                    // 조각 하나로 완성이 되는 경우
+                    if(i+1-strs[j].length() == 0) {
+                        dp[i] = 1;
+                    }
+                    // 조각의 길이가 i번째 문자까지의 문자열보다 긴 경우 조각을 사용할 수 없습니다.
+                    // ex. t="apple", i=1, c='p'일때, "app"는 길이3, i번째 까지의 문자열은 길이2(초과)
+                    else if(i+1-strs[j].length() < 0) {
+                        
+                    }
+                    
+                    
+                }
+            }
         }
-        
-        Collections.sort(list, (o1, o2) -> o2.length()-o1.length());
-        
-        System.out.println(list);
-        
-        // String test = "nabnabn";
-        // test = test.replaceAll("n", "");
-        // System.out.println(test);
-        // test = test.replaceAll("abab", "");
-        // System.out.println(test);
-        // getMin(0, t, set);
-        dfs(0, 0, t ,list);
         
         // 초기값 그대로라면 주어진 단어를 조각들을 이용하여 문장을 완성할 수 없음을 의미합니다.
-        return answer == 101 ? -1 : answer;
-    }
-    public void dfs(int result, int depth, String t, List<String> list) {
-        System.out.println(t);
-        if(t.equals(" ".repeat(length))) {
-            System.out.println(result);
-            answer = Math.min(answer, result);
-        }
-        else if(depth == list.size()) {
-            
-        }
-        else {
-            String str = list.get(depth);
-            if(t.contains(str)) {
-                String next_t = t.replaceAll(str, " ".repeat(str.length()));
-                int count = (t.length()-t.replaceAll(str, "").length())/str.length();
-                dfs(result+count, depth+1, next_t, list);
-            }
-            dfs(result, depth+1, t, list);
-        }
-    }
-    // 주어진 단어t를 두 단어로 쪼개어 사용하는 단어 조각 개수의 최소값을 구하는 함수입니다.
-    public void getMin(int result, String t, HashSet<String> set) {
-        // 빈 단어인 경우 단어 조각을 찾을 필요가 없으므로 0을 반환합니다.
-        if(t.equals("")) {
-            answer = Math.min(answer, result);
-        }
-        
-        // 주어진 단어 t를 최소 1개부터 t의 길이 만큼까지 두 단어로 분리합니다. (ex. t="abc", f_word = "abc", b_word = "")
-        for(int i = 1 ; i <= t.length() ; i++) {
-            String f_word = t.substring(0, i);
-            // 분리한 앞단어의 단어 조각이 존재하는 경우에만 뒷단어의 조각 개수를 구합니다.
-            if(set.contains(f_word)) {
-                String b_word = t.substring(i, t.length());
-                getMin(result+1, b_word, set);
-            }
-        }
+        return answer;
     }
 }
 
 /*
+1.
 단어를 쪼개어 확인하는 방법
 주어진 단어 t의 길이가 n이라 할때
 front 0~mid
@@ -79,6 +48,7 @@ back mid+1~end
 front 단어 조각에 들어있으면 분리 가능하며 없다면 분리 불가능
 시간초과
 -----------------------------------------
+2.
 단어 조각 목록을 기준으로 조합을 구함
 단어 조각이 긴것부터 사용하는 것이 유리
 ["app","ap","p","l","e","ple","pp"]
@@ -86,6 +56,16 @@ apple
 app,l,e보다 ap,ple가 적은 조각을 사용
 app를 선택한 경우와 선택하지 않은 경우
 ple를 선택한 경우와 선택하지 않은 경우 ...
-
+반례 존재 nabnabn
+-----------------------------------------
+3.
+1번 과정을 DP로 접근
+["ba","na","n","a"]
+banana
+dp[0] = b = invalid (b로끝나는 문자가없음)
+dp[1] = b+a or ba = dp[0]+1 or 1 (a로 끝나는 문자는 a, ba 두 개)
+dp[2] = ban = ba+n = 2 (n으로 끝나는 문자는 n 한 개)
+dp[3] = bana = ban+a or ba+na = dp[2]+1 or dp[1]+1 최소값 2 (a로 끝나는 문자는 na, a 두 개, 각 길이만큼 뺀 dp값과 더하여 비교)
+dp[4] = banan = bana+n 
 
 */
