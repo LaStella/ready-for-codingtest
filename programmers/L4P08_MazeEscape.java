864edc1d8ea16886963447d0c9a5bbf42eea42b9
Jingyu Ham <jingyu.ham94@gmail.com>
2022-06-05 23:31:44 +0900

    modify "Maze Escape" test -incomplete(design using dijkstra algorithm)

--- programmers/L4P08_MazeEscape.java
+++ programmers/L4P08_MazeEscape.java
@@ -7,61 +7,16 @@ class Solution {
     public int solution(int n, int start, int end, int[][] roads, int[] traps) {
         int answer = 0;
         
-        int[][] route = new int[n+1][n+1];
-        
-        for(int[] road : roads) {
-            route[road[0]][road[1]] = road[2];
-        }
-        
-        Queue<Node> q = new LinkedList<>();
-        q.offer(new Node(start, 0));
-        // System.out.println(q);
-        // q.poll();
-        // System.out.println(q);
-        // System.out.println(q.peek().index);
-        while(!q.isEmpty()) {
-            Node p_node = q.poll();
-            int p_index = p_node.index;
-            int p_time = p_node.time;
-            
-            if(p_index == end) {
-                System.out.println(p_time);
-                break;
-            }
-            
-            for(int i = 1 ; i < n+1 ; i++) {
-                if(route[p_index][i] != 0) {
-                    q.offer(new Node(i, p_time+route[p_index][i]));
-                }
-            }
-        }
+        int[] dist = new int[n+1];
         
         
         
         return answer;
     }
-    
-    public boolean isTrap(int index, int[] traps) {
-        for(int i = 0 ; i < traps.length ; i++) {
-            if(index == traps[i]) {
-                return true;
-            }
-        }
-        
-        return false;
-    }
-    
-    class Node {
-        int index;
-        int time;
-        public Node(int index, int time) {
-            this.index = index;
-            this.time = time;
-        }
-    }
 }
 
 /*
+1.
 2차 배열에 모든 경로를 저장
 int[][] route, route[a][b]는 a에서 b로 연결되는 경로 
 
@@ -74,6 +29,13 @@ route[b][a] = temp;
 bfs를 이용하여 탐색
 문제발생> 트랩에 의해 바뀌는 간선때문에 bfs를 이용할 수 없다.
 ------------------------------
-
-
+2.
+다익스트라 이용
+출발 정점에서 연결된 정점을 방문
+> 방문 시 출발지에서 목적지 정점까지 걸리는 시간이 작은 값을 저장, int[] dist배열 이용, dist[2] = 출발지에서 2번 정점까지 걸리는 시간
+> 누적 시간은 이동한 정점에 저장합니다. Node 객체 이용
+> 누적 시간이 가장 적은 정점을 뽑아 다음 정점으로 이동합니다.
+> 정점이 end에 도달하면 dist[end]와 비교하여 최소값을 저장합니다.
+> trap의 발동 여부를 항상 저장하여 다음 정점으로 정보를 넘겨줍니다.
+    > ex. 입출력예2번에서 2번 트랩을 밟을 경우, 1번으로 이동하는 경우와 3번으로 이동하는 경우가 존재, 3번으로 이동할 경우 트랩이 발동하며, 1번으로 이동한 경우 트랩은 발동하지 않습니다. 서로 다른 경우이므로 트랩의 발동 유무는 공유하지 않아야합니다.
 */
