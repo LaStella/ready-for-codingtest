575b16f081902977e5cc24bc8b4e99987dbf2ddb
Jingyu Ham <jingyu.ham94@gmail.com>
2022-06-16 23:43:41 +0900

    modify "Caving" test -incomplete(1 timeout in efficiency test)

--- programmers/L4P12_Caving.java
+++ programmers/L4P12_Caving.java
@@ -8,11 +8,15 @@ class Solution {
         boolean answer = true;
         boolean[] visited = new boolean[n];
         
-        // 통로를 2차 배열로 저장합니다.
-        boolean[][] route = new boolean[n][n];
+        // 통로를 리스트에 저장합니다.
+        // route.get(i)는 i번 방과 연결된 모든 통로가 저장됩니다.
+        ArrayList<ArrayList<Integer>> route = new ArrayList<>();
+        for(int i = 0 ; i < n ; i++) {
+            route.add(new ArrayList<>());
+        }
         for(int[] p : path) {
-            route[p[0]][p[1]] = true;
-            route[p[1]][p[0]] = true;
+            route.get(p[0]).add(p[1]);
+            route.get(p[1]).add(p[0]);
         }
         
         // 모든 순서를 맵으로 저장합니다. (key : 방 번호, value : key를 들어가기 위해 먼저 방문해야하는 방번호)
@@ -23,6 +27,7 @@ class Solution {
         
         // 큐에는 현재 방문 가능한 방 번호가 들어갑니다.
         Queue<Integer> q = new LinkedList<>();
+        visited[0] = true;
         q.add(0);
         // 큐에 있는 방을 순회한 횟수를 저장합니다.
         int count = 0;
@@ -48,9 +53,9 @@ class Solution {
             visited[p_index] = true;
             
             // 현재 방과 연결된 다른 방들을 큐에 넣습니다.
-            for(int i = 0 ; i < n ; i++) {
-                // 방문하지 않은 방이며 통로가 존재하는 방인지 확인합니다.
-                if(!visited[i] && route[p_index][i]) {
+            for(int i : route.get(p_index)) {
+                // 이미 방문한 방은 넣지않습니다.
+                if(!visited[i]) {
                     q.add(i);
                 }
             }
@@ -69,5 +74,8 @@ order를 map에 저장합니다.
 방문할 수 있다면 0번과 연결된 모든 방을 큐에 넣습니다. (1,3,7)
 방문할 수 없다면 0번을 다시 큐에 넣습니다.
 
-
+문제발생> 방과 방 사이의 통로들을 2차 boolean배열 형태로 저장하였으나, n의 크기가 커짐에 따라 메모리가 초과되는 문제가 발생하였습니다.
+이를 해결하기 위해 각 통로들을 ArrayList를 이용하여 저장하였습니다.
+문제발생> 마지막 효율성 테스트에서 시간초과가 발생하였습니다. 입장 순서가 존재하는 방인 경우 먼저 방문해야 하는 방을 방문하지 않은 경우 다시 큐에 넣게되는데,
+이때 먼저 방문해야 하는 방을 방문하여도 큐에서 해당 방의 차례가 오기까지 한참 걸릴 수 있는 문제가 있습니다.
 */
