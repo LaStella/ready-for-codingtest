d2947bb82f7f6a772b255103b87efac4b1d610a2
Jingyu Ham <jingyu.ham94@gmail.com>
2022-06-20 23:15:07 +0900

    modify "Tube's Blind Date" test -incomplete(using priorityqueue, time out)

--- programmers/L4P13_BlindDate.java
+++ programmers/L4P13_BlindDate.java
@@ -7,41 +7,41 @@ class Solution {
     public int[] solution(int m, int n, int s, int[][] time_map) {
         int[] answer = new int[2];
         
-        // 각 칸의 모든 이동 횟수를 2500으로 초기화합니다.
+        // 각 칸의 모든 이동 횟수를 임의의 큰 숫자로 초기화합니다.
         int[][][] board = new int[m][n][2];
         for(int r = 0 ; r < m ; r++) {
             for(int c = 0 ; c < n ; c++) {
-                board[r][c][0] = 2500;
+                board[r][c][0] = Integer.MAX_VALUE;
+                board[r][c][1] = Integer.MAX_VALUE;
             }
         }
         
-        Queue<Node> q = new LinkedList<>();
+        Queue<Node> q = new PriorityQueue<>();
         q.add(new Node(0, 0, 0, 0));
         int[] dr = {-1, 1, 0, 0};
         int[] dc = {0, 0, -1, 1};
         
         while(!q.isEmpty()) {
+            // 큐에서 가장 적은 이동 횟수를 가진 노드를 뽑습니다.
+            // 같은 이동 횟수라면 더 적은 대화시간을 가지는 노드를 뽑습니다.
             Node p_node = q.poll();
             int r = p_node.r;
             int c = p_node.c;
             
+            // 도착지에 도착하면 중단합니다.
+            if(r == m-1 && c == n-1) {
+                answer[0] = p_node.moveDistance;
+                answer[1] = p_node.sumOfTalkTime;
+                break;
+            }
             // 현재 좌표에 도착했을 때의 이동 횟수가 기존것보다 많으면 넘어갑니다.
             if(board[r][c][0] < p_node.moveDistance && board[r][c][1] < p_node.sumOfTalkTime) {
                 continue;
             }
             
-            // 이동 횟수가 더 적으면 현재의 이동 횟수와 대화 시간으로 저장합니다.
-            if(board[r][c][0] > p_node.moveDistance) {
-                board[r][c][0] = p_node.moveDistance;
-                board[r][c][1] = p_node.sumOfTalkTime;
-            }
-            // 이동 횟수가 같으면 대화 시간이 더 적은 것으로 저장합니다.
-            else {
-                board[r][c][1] = Math.min(board[r][c][1], p_node.sumOfTalkTime);
-            }
-            
-            // 도착지에 도착했다면 더 이상 이동할 필요가 없습니다.
-            if(r == m-1 && c == n-1) continue;
+            // 현재 좌표의 이동 횟수와 대화 시간을 각각 더 작은 값으로 저장합니다.
+            board[r][c][0] = Math.min(board[r][c][0], p_node.moveDistance);
+            board[r][c][1] = Math.min(board[r][c][1], p_node.sumOfTalkTime);
             
             // 현재 좌표를 기준으로 상, 하, 좌, 우로 움직인 새로운 좌표를 만들어 탐색합니다.
             for(int i = 0 ; i < 4 ; i++) {
@@ -56,9 +56,6 @@ class Solution {
                 }
             }
         }
-        
-        answer[0] = board[m-1][n-1][0];
-        answer[1] = board[m-1][n-1][1];
       
         return answer;
     }
@@ -68,7 +65,7 @@ class Solution {
     }
 }
 
-class Node {
+class Node implements Comparable<Node> {
     int r;
     int c;
     int moveDistance;
@@ -78,6 +75,16 @@ class Node {
         this.c = c;
         this.moveDistance = m;
         this.sumOfTalkTime = s;
+    }
+    
+    @Override
+    public int compareTo(Node other) {
+        if(this.moveDistance == other.moveDistance) {
+            return this.sumOfTalkTime - other.sumOfTalkTime;
+        }
+        else {
+            return this.moveDistance - other.moveDistance;
+        }
     }
 }
 
@@ -89,6 +96,11 @@ class Node {
 작지 않으면 넘어갑니다.
 문제발생> 큐에서 뽑은 노드가 도착지점이면 종료하였는데, 이후로 더 적은 대화시간을 가진 노드가 도착지에 도착할 수 있습니다.
 따라서 종료 조건을 없애고, 큐에 더 이상 노드가 존재하지 않을 때까지 반복합니다.
+
+문제발생> 테스트 실패, 이동 횟수를 첫번째로 비교하고 두번째로 대화시간을 비교하므로 대화시간이 더 적지만 이동횟수가 많은 노드가 제외되는 문제가 발생한것으로 생각됩니다.
+노드를 우선순위큐를 이용하여 정렬하며, 정렬 조건의 최우선은 이동횟수, 이동횟수가 같다면 대화시간을 비교하여 정렬하도록 하였습니다.
+
+문제발생> 테스트케이스 시간초과
 
 
 */
