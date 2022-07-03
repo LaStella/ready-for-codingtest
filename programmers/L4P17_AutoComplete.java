6763e9511068876d8a02a480d69395c17743e7af
Jingyu Ham <jingyu.ham94@gmail.com>
2022-07-03 23:59:12 +0900

    add "Auto Complete" test -incomplete(designing)

---
+++ programmers/L4P17_AutoComplete.java
@@ -0,0 +1,14 @@
+// 자동 완성
+// https://programmers.co.kr/learn/courses/30/lessons/17685?language=java
+
+class Solution {
+    public int solution(String[] words) {
+        int answer = 0;
+        return answer;
+    }
+}
+/* 
+지난 가사검색에서 사용한 Trie구조를 사용해야 하는것으로 보입니다.
+
+
+*/
