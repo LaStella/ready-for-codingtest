f60cf48b4e53f94de90ff9f30d159ce18cd746e3
Jingyu Ham <jingyu.ham94@gmail.com>
2022-07-06 23:59:38 +0900

    add "Even Row Count" test -incomplete(cant solve, looking others solution)

--- programmers/L4P18_EvenRowCount.java
+++ programmers/L4P18_EvenRowCount.java
@@ -8,13 +8,7 @@ class Solution {
         int n = a.length;
         int m = a[0].length;
         
-        for(int c = 0 ; c < m ; c++) {
-            int count = 0;
-            for(int r = 0 ; r < n ; r++) {
-                if(a[r][c] == 1) count++;
-            }
-            // n! / ( (n-count)! * count! )
-        }
+        
         
         return answer;
     }
@@ -25,5 +19,10 @@ class Solution {
 2. 1에서 계산한 0과 1의 개수로 배치 가능한 b의 열의 경우의 수를 구합니다.
     2-1. (행의 갯수!) / (0의 개수!) * (1의 개수!)
 문제발생 : 너무 많은 계산시간을 요구합니다.
+
+dp로 접근
+너무 어려워서 풀이글을 보았습니다.
+풀이글을 이해를 못하여 현재 이해하려고 보는 중
+
 
 */
