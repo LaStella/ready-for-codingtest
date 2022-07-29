74aff55305340afc9c0a36ed26cefc796b17b50d
Jingyu Ham <jingyu.ham94@gmail.com>
2022-07-30 00:03:59 +0900

    modify and rename "Down" -> "GoDown" test -complete

--- baekjoon/2096_GoDown.java
+++ baekjoon/2096_GoDown.java
@@ -33,16 +33,19 @@ public class Main {
 
         for(int r = 1 ; r < n ; r++) {
             for(int c = 0 ; c < 3 ; c++) {
-                int max1 = 0;
-                int min1 = Integer.MAX_VALUE;
                 // 현재행을 기준으로 바로 위의 행(r-1)에서 내려옵니다.
                 // 위의 행에서 내려올 때는 인접한 열로 내려올 수 있으므로 c를 기준으로 c-1과 c+1로 이동이 가능합니다.
-                // 범위를 벗어나는 c-1과 c+1 
+                // 범위를 벗어나는 c-1과 c+1은 계산하지 않습니다.
+                
+                // c번째 열과 c-1번째 열의 값을 비교합니다.
+                int max1 = 0;
+                int min1 = Integer.MAX_VALUE;
                 if(c-1 >= 0) {
                     max1 = Math.max(dp[r-1][c-1][0] + board[r][c], dp[r-1][c][0] + board[r][c]);
                     min1 = Math.min(dp[r-1][c-1][1] + board[r][c], dp[r-1][c][1] + board[r][c]);
                 }
 
+                // c번째 열과 c+1번째 열의 값을 비교합니다.
                 int max2 = 0;
                 int min2 = Integer.MAX_VALUE;
                 if(c+1 <= 2) {
@@ -50,6 +53,7 @@ public class Main {
                     min2 = Math.min(dp[r-1][c][1] + board[r][c], dp[r-1][c+1][1] + board[r][c]);
                 }
 
+                // 위에서 구한 두 값을 비교하여 저장합니다.
                 dp[r][c][0] = Math.max(max1, max2);
                 dp[r][c][1] = Math.min(min1, min2);
             }
