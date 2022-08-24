acc618366f088ef40844d05c780c0a217c0a9c4e
Jingyu Ham <jingyu.ham94@gmail.com>
2022-08-25 00:16:40 +0900

    modify "Plum Tree" test -complete

--- baekjoon/2240_PlumTree.java
+++ baekjoon/2240_PlumTree.java
@@ -20,20 +20,35 @@ public class Main {
         int tree_index = 0;
         int jadu_index = 0;
         for (int i = 1 ; i <= t ; i++) {
-            tree_index = Integer.parseInt(st.nextToken());
+            tree_index = Integer.parseInt(br.readLine());
 
             for (int j = 0 ; j <= w ; j++) {
-                jadu_index = j % 2 == 0 ? 1 : 2;
-                if(jadu_index == tree_index) {
-                    dp[i][j] = Math.max(dp[i-1][j]+1, dp[i-1][j-1]);
+                // 자두가 움직이지 않은 경우
+                if (j == 0) {
+                    // 자두의 위치는 1번 나무에서 시작하므로 1번 나무에서 떨어지는 자두만 받을 수 있습니다.
+                    dp[i][j] = tree_index == 1 ? dp[i-1][j]+1 : dp[i-1][j];
                 }
+
                 else {
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-1]+1);
+                    // 자두의 위치는 움직인 횟수가 홀수면 2번나무, 짝수면 1번나무입니다.
+                    jadu_index = j % 2 == 0 ? 1 : 2;
+
+                    // 자두의 위치와 자두가 떨어질 나무의 번호가 같아야 자두를 받을 수 있습니다.
+                    if (jadu_index == tree_index) {
+                        // 이전 시간(i-1)에 자두의 현재 위치에서 자두를 +1한 경우와 반대 나무에 위치하여 자두를 못받은 경우를 비교하여 더 큰 값을 저장합니다.
+                        dp[i][j] = Math.max(dp[i-1][j]+1, dp[i-1][j-1]);
+                    }
+                    // 자두의 위치와 자두가 떨어질 나무의 번호가 다르다면 반대편에 위치하였을 경우 자두를 받을 수 있습니다.
+                    else {
+                        // 위와 마찬가지로 이전 시간(i-1)에 자두가 반대 나무에 있을 경우 +1과 자두의 현재 위치에서 자두를 못받은 경우를 비교합니다.   
+                        dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-1]+1);
+                    }
                 }
             }
         }
 
         int max = 0;
+        // 끝나는 시간은 t시간으므로 자두가 움직인 횟수에 따라 최대값을 찾습니다.
         for (int i = 0 ; i <= w ; i++) {
             max = Math.max(max, dp[t][w]);
         }
