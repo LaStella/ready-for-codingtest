23a332a18fc7a81c77ce7117fce7b9b16e0c7e19
Jingyu Ham <jingyu.ham94@gmail.com>
2022-09-28 23:03:37 +0900

    modify "Near Bit Count" test -complete

--- baekjoon/2698_NearBitCount.java
+++ baekjoon/2698_NearBitCount.java
@@ -7,14 +7,41 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static int MOD = 1000000007;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int T = Integer.parseInt(br.readLine());
 
-        while (T-- > 0) {
+        // dp[i][j][k] : 길이가 i이며 인접한 비트의 수가 j, 수열의 마지막이 k인 경우의 수
+        int[][][] dp = new int[101][101][2];
 
+        dp[1][0][0] = 1;
+        dp[1][0][1] = 1;
+
+        for (int i = 2 ; i <= 100 ; i++) {
+            for (int j = 0 ; j <= 100 ; j++) {
+                if (j == 0) {
+                    // 인접한 비트가 없는 경우 1로 끝나는 경우의 수는 i-1길이에 0으로 끝나는 경우의 수입니다.
+                    // i-1길이에 1로 끝나는 경우 뒤에 1을 붙이게되면 인접한 비트가 늘어나므로 더할 수 없습니다.
+                    dp[i][j][1] = dp[i-1][j][0];
+                }
+                else {
+                    // 1. i-1길이에 j-1개의 인접한 비트를 가지며 1로 끝나는 경우 뒤에 1을 붙이면 i길이에 j개의 인접한 비트를 가지며 1로 끝나는 수열이 됩니다.
+                    // 2. i-1길이에 j개의 인접한 비트를 가지며 0으로 끝나는 경우 뒤에 1을 붙이면 i길이에 인접한 비트는 늘지 않으며 1로 끝나는 수열이 됩니다.
+                    dp[i][j][1] = dp[i-1][j-1][1] + dp[i-1][j][0];
+                }
+                // 0으로 끝나는 경우의 수는 i-1길이에서 구할 수 있는 모든 경우의 수가 해당됩니다.
+                dp[i][j][0] = dp[i-1][j][0] + dp[i-1][j][1];
+            }
+        }
+
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+
+            System.out.println(dp[n][k][0]+dp[n][k][1]);
         }
     }
 }
