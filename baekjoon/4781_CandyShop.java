d905f25162088cbecc6525f14f0988a4c6ea16b9
Jingyu Ham <jingyu.ham94@gmail.com>
2022-10-26 21:40:56 +0900

    modify "Candy Shop" test -fix save error

--- baekjoon/4781_CandyShop.java
+++ baekjoon/4781_CandyShop.java
@@ -1,49 +1,69 @@
-// 숫자카드
-// https://www.acmicpc.net/problem/2591
+// 사탕 가게
+// https://www.acmicpc.net/problem/4781
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // i번째 숫자를 나타내기 위하여 맨 앞에 0을 붙여 길이를 1 늘립니다.
-        String[] s = ("0"+br.readLine()).split("");
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            // m은 소수점 둘째자리를 가진 실수형태로 주어집니다.
+            // 실수 자료형인 double에 100을 곱하여 int형으로 변환할 경우 Rounding Error가 발생하여 정확하지않은 값으로 변환되는 문제가 있습니다.
+            // double형 데이터를 BigDecimal형으로 바꾸어 100을 곱한 후 이를 int형으로 변환합니다.
+            int m = BigDecimal.valueOf(Double.parseDouble(st.nextToken())).multiply(new BigDecimal(100)).intValue();
 
-        // dp[i] : i번째 수까지 만드는 경우의 수
-        int[] dp = new int[s.length+1];
-        // 첫번째 수를 만드는 경우의 수는 1개뿐입니다.
-        dp[1] = 1;
-        // n : 이어지는 두 숫자를 int형으로 변환
-        int n = Integer.parseInt(s[1]+s[2]);
-        // 1~34까지 카드가 존재하며 이 중 두자리 수는 10~34입니다.
-        // 10, 20, 30은 0카드가 존재하지 않으므로 만드는 경우의 수는 1개뿐입니다.
-        if (11 <= n && n <= 34 && n != 20 && n != 30) dp[2] = 2;
-        else dp[2] = 1;
+            // n과 m의 입력이 0이라면 중단합니다.
+            if (n == 0 && m == 0) break;
 
-        // 각 자리수에 따라 만드는 경우의 수를 계산합니다.
-        for (int i = 3 ; i <= s.length-1 ; i++) {
-            // i-1번째 수와 i번째 수를 붙여 만든 수를 n으로 저장합니다.
-            n = Integer.parseInt(s[i-1]+s[i]);
-            // 두 자리수의 n이 카드로 존재하는 수인 경우 두 자리수를 뺀 dp[i-2]에 n이 적힌 카드를 더해 만드는 경우의 수가 더해집니다.
-            if (10 <= n && n <= 34) {
-                dp[i] += dp[i-2];
+            // v[i] : i번 사탕의 칼로리
+            // c[i] : i번 사탕의 비용
+            int[] v = new int[n+1];
+            int[] c = new int[n+1];
+
+            for (int i = 1 ; i <= n ; i++) {
+                st = new StringTokenizer(br.readLine());
+                v[i] = Integer.parseInt(st.nextToken());
+                c[i] = BigDecimal.valueOf(Double.parseDouble(st.nextToken())).multiply(new BigDecimal(100)).intValue();
             }
-            // 한 자리수의 카드로 만드는 경우 i번째 수를 뺀 dp[i-1]에 i번째 수가 적힌 카드를 더해 만드는 경우의 수가 더해집니다.
-            // i번째 수가 0이라면 0이 적힌 카드가 존재하지 않으므로 만들 수 없습니다.
-            if (Integer.parseInt(s[i]) != 0) {
-                dp[i] += dp[i-1];
+
+            // dp[i] : i금액으로 만들 수 있는 최대 칼로리
+            int[] dp = new int[m+1];
+
+            // 각 사탕에 대해 금액별로 최대 칼로리를 계산합니다.
+            for (int i = 1 ; i <= n ; i++) {
+                for (int j = 1 ; j <= m ; j++) {
+                    // 금액 j가 사탕보다 많거나 같은 경우 사탕을 구매할 수 있습니다.
+                    // i번 사탕의 칼로리와 남은 금액으로 만들 수 있는 최대 칼로리의 합이 j금액으로 만들 수 있는 칼로리가 됩니다.
+                    // 같은 금액이라도 만들 수 있는 칼로리는 다르므로 최대값을 저장합니다.
+                   if (c[i] <= j) {
+                        dp[j] = Math.max(dp[j], dp[j-c[i]] + v[i]);
+                    }
+                }
             }
+
+            System.out.println(dp[m]);
         }
-        
-        System.out.println(dp[s.length-1]);
     }
 }
 
 /*
-문제발생1. 0을 고려하지 않아 만들 수 없는 경우의 수가 포함되는 경우가 발생하였습니다.
-문제발생2. 문제발생1과 유사하며 10, 20, 30에서의 0을 고려하지 않아 문제가 발생하였습니다. 
+배낭문제와 동일하게 풀 수 있습니다.
+배낭문제와 다른 점은 물건의 중복 선택이 가능하다는 점입니다.
+dp[i] : i금액으로 살 수 있는 사탕의 최대 칼로리
+금액을 1부터 소지금액인 m까지 늘리며 칼로리의 최대값을 계산합니다.
+
+1번~n번 사탕까지 존재하므로 2중 for문을 사용하여 사탕과 금액에 대해서 최대 칼로리를 계산합니다.
+사탕의 종류 i, 금액을 j라고 한다면 i번 사탕의 비용이 j보다 적은 경우에 구매할 수 있습니다.
+이때 남은 금액(j-cost[i])으로 살 수 있는 최대 칼로리와 i번 사탕의 칼로리의 합이 j금액으로 살 수 있는 칼로리가 됩니다.
+dp[j] = dp[j-cost[i]] + value[i]
+
+문제발생. 입력으로 주어지는 실수에 대해서 *100을 하여 int형으로 계산하였으나 실수표현의 부정확성때문에 발생하는 오류(Rounding Error or Round-off Error)가 발생하였습니다.
+        BigDecimal을 사용하여 문제를 해결하였습니다.
  */
