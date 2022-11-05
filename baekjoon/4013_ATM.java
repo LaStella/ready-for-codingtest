21d0d16226d8cc80264ddd03f4bd9e86dc508c05
Jingyu Ham <jingyu.ham94@gmail.com>
2022-11-05 23:43:04 +0900

    add "ATM" test -incomplete

---
+++ baekjoon/4013_ATM.java
@@ -0,0 +1,22 @@
+// ATM
+// https://www.acmicpc.net/problem/4013
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
+
+/*
+오랜시간 고민했으나 해결방법을 찾지못하여 참고
+강한 결합 요소 (Strongly Connected Componet) 추출 알고리즘을 알게되었습니다.
+--------------------------------------
+
+ */
