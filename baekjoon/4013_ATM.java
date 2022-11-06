2ea5baef90938d621f1b6534c3f13b5c919d27b3
Jingyu Ham <jingyu.ham94@gmail.com>
2022-11-06 23:50:33 +0900

    modify "ATM" test -incomplete(still designing)

--- baekjoon/4013_ATM.java
+++ baekjoon/4013_ATM.java
@@ -4,12 +4,57 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigDecimal;
 import java.util.*;
 
 public class Main {
+    static int[] atm;
+    static boolean[] restaurant;
+    static ArrayList<ArrayList<Integer>> edge_list = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        // N개의 교차로를 생성합니다.
+        for (int i = 0 ; i <= N ; i++) {
+            edge_list.add(new ArrayList<>());
+        }
+
+        // M개의 도로를 입력받아 저장합니다.
+        for (int i = 0 ; i < M ; i++) {
+            st = new StringTokenizer(br.readLine());
+            int edge_start = Integer.parseInt(st.nextToken());
+            int edge_end = Integer.parseInt(st.nextToken());
+
+            // 시작 교차로에서 끝 교차로로 가는 도로를 추가하여 두 교차로를 이어줍니다.
+            edge_list.get(edge_start).add(edge_end);
+        }
+
+        atm = new int[N+1];
+
+        // 각 ATM에 들어있는 돈을 저장합니다.
+        for (int i = 1 ; i < N ; i++) {
+            atm[i] = Integer.parseInt(br.readLine());
+        }
+
+        st = new StringTokenizer(br.readLine());
+
+        // 시작 교차로와 레스토랑의 개수를 입력받습니다.
+        int start = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+
+        restaurant = new boolean[N+1];
+
+        // 레스토랑이 있는 교차로는 true를 저장합니다.
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0 ; i < P ; i++) {
+            restaurant[Integer.parseInt(st.nextToken())] = true;
+        }
+
 
     }
 }
