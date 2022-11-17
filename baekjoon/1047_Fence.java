ffab002ea0efd9ad6ff46b99ebfc04f8e2f863c5
Jingyu Ham <jingyu.ham94@gmail.com>
2022-11-18 02:34:38 +0900

    백준 1047 울타리 문제 수정
    - Tree 객체의 x, y 값만 사용하므로 정렬된 x리스트와 y리스트는 각각 x값과 y값만을 저장하는 Integer 타입 리스트로 변경하였습니다.
    - 울타리 객체를 만들었습니다.
      - 울타리를 만드는데 필요한 나무 개수를 구하는 메소드를 만들었습니다.
      - 울타리의 안에 해당 나무가 들어오는지 여부를 구하는 메소드를 만들었습니다.
    - 우선순위큐를 사용하여 울타리 밖에 있는 나무를 길이 순으로 저장합니다.
    - x1 == x2인 경우 필요한 나무개수를 구하지 못하는 오류를 발견했습니다.
    
    채점 결과 : 미완성

--- baekjoon/1047_Fence.java
+++ baekjoon/1047_Fence.java
@@ -20,24 +20,51 @@ public class Main {
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
             int length = Integer.parseInt(st.nextToken());
+            
             Tree tree = new Tree(x, y, length);
             treeList.add(tree);
         }
 
-        List<Tree> sortedTreeListByX = treeList.stream()
-                .sorted((o1, o2) -> o1.x - o2.x)
+        List<Integer> sortedTreeListByX = treeList.stream()
+                .map(Tree::getX)
+                .sorted(((o1, o2) -> o1 - o2))
                 .toList();
-        List<Tree> sortedTreeListByY = treeList.stream()
-                .sorted((o1, o2) -> o1.y - o2.y)
+        List<Integer> sortedTreeListByY = treeList.stream()
+                .map(Tree::getY)
+                .sorted((o1, o2) -> o1 - o2)
                 .toList();
 
         for (int x1 = 0 ; x1 < n ; x1++) {
-            for (int x2 = n-1 ; x2 > x1 ; x2--) {
-                
+            for (int x2 = n-1 ; x2 >= x1 ; x2--) {
+                for (int y1 = 0 ; y1 < n ; y1++) {
+                    for (int y2 = n-1 ; y2 >= y1 ; y2--) {
+                        Fence fence = new Fence(sortedTreeListByX.get(x1), sortedTreeListByX.get(x2), sortedTreeListByY.get(y1), sortedTreeListByY.get(y2));
+
+                        PriorityQueue<Tree> outRangeTreeQueue = new PriorityQueue<>(((o1, o2) -> o2.getLength() - o1.getLength()));
+
+                        for (Tree tree : treeList) {
+                            if (!fence.isInRange(tree.getX(), tree.getY())) {
+                                outRangeTreeQueue.add(tree);
+                            }
+                        }
+
+
+                        int needCount = fence.getNeedCount();
+                        int cutCount = 0;
+                        while (needCount > 0 && !outRangeTreeQueue.isEmpty()) {
+                            needCount -= outRangeTreeQueue.poll().getLength();
+                            cutCount++;
+                        }
+
+                        if (needCount <= 0) System.out.println(cutCount);
+
+                    }
+                }
             }
         }
 
-        System.out.println("!");
+
+        System.out.println("!"); //breakpoint
 
 
     }
@@ -51,6 +78,37 @@ class Tree {
         this.y = y;
         this.length = length;
     }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getLength() {
+        return length;
+    }
+}
+
+class Fence {
+    int x1, x2, y1, y2;
+
+    public Fence(int x1, int x2, int y1, int y2) {
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y1 = y1;
+        this.y2 = y2;
+    }
+
+    public int getNeedCount() {
+        return (2 * (x2 - x1)) + (2 * (y2 - y1));
+    }
+
+    public boolean isInRange(int x, int y) {
+        return x1 <= x && x <= x2 && y1 <= y && y <= y2;
+    }
 }
 
 
@@ -63,7 +121,7 @@ class Tree {
 
 각 4 꼭지점에 대해 for문을 사용하여 크기를 점차 줄입니다.
 우선 각각 x값과 y값의 크기순으로 정렬된 리스트 2개를 만들어 각 꼭지점을 저장합니다.
-울타리를 이루는 4 꼭지점은 각각 (x1,y1) (x1, y2) (x2, y1) (x2, y2) 라고 한다면 
+울타리를 이루는 4 꼭지점은 각각 (x1,y1) (x1, y2) (x2, y1) (x2, y2) 라고 한다면
 x1은 x 최솟값부터 시작, x2는 x최댓값부터 시작하여 1씩 줄이며 리스트에서 다음값을 (x1은 최솟값, x2는 최댓값)을 가져옵니다.
 y값도 마찬가지입니다.
 
