58eee7c744f716aa80110a7224991077bc46e1b2
Jingyu Ham <jingyu.ham94@gmail.com>
2022-12-12 22:01:36 +0900

    백준 1050 물약 문제 기능 추가
    int형 값의 범위 초과가 발생하므로 정수형이아닌 BigInteger형으로 모두 변경하였습니다.
    recipe에 따라 제조하는 과정과 recipeCost를 계산하는 과정을 분리하여 메소드로 만들었습니다.
    채점실패의 반례를 찾았습니다. (주석)
    
    채점 결과 : 실패

--- baekjoon/1050_Potion.java
+++ baekjoon/1050_Potion.java
@@ -4,21 +4,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        Map<String, Integer> map = new HashMap<>();
+        Map<String, String> map = new HashMap<>();
         for (int i = 0 ; i < N ; i++) {
             st = new StringTokenizer(br.readLine());
-            map.put(st.nextToken(), Integer.parseInt(st.nextToken()));
+            map.put(st.nextToken(), st.nextToken());
         }
 
         Queue<String> queue = new LinkedList<>();
@@ -26,6 +26,13 @@ public class Main {
             queue.add(br.readLine());
         }
 
+        map = makePotion(queue, map);
+
+        System.out.println(map.getOrDefault("LOVE", "-1"));
+    }
+
+    // 큐에 저장된 제조법에 따라 재료를 만드는 함수입니다.
+    public static Map<String, String> makePotion(Queue<String> queue, Map<String, String> map) {
         int cycleCount = 0;
         // 더이상 제조법이 없거나 제조할 수 있는 물약이 없는 경우 종료합니다.
         while(!queue.isEmpty() && queue.size() != cycleCount) {
@@ -34,33 +41,50 @@ public class Main {
             String recipeName = recipeArray[0];
             String[] material = recipeArray[1].split("\\+");
 
-            int recipeCost = 0;
-            boolean isComplete = true;
-            for (String s : material) {
-                int materialCount = Integer.parseInt(s.replaceAll("[^0-9]", ""));
-                String materialName = s.replaceAll("[0-9]", "");
-                // 찾는 재료가 없다면 제조를 중단하고 제조법을 다시 큐에 넣습니다.
-                if (!map.containsKey(materialName)) {
-                    isComplete = false;
-                    break;
-                }
+            String recipeCost = calcRecipeCost(material, map);
 
-                recipeCost += materialCount*map.get(materialName);
+            // 제조가 불가능한 경우 제조법을 다시 큐에 넣습니다.
+            if (recipeCost.equals("-1")) {
+                cycleCount++;
+                queue.add(recipe);
+                continue;
             }
 
-            cycleCount++;
-
-            // 새로 재료가 추가되는 경우
-            if (isComplete) {
-                // 이미 추가되어있는 재료라면 최소값을 저장으로 수정합니다.
-                if (map.putIfAbsent(recipeName, recipeCost) != null) {
-                    map.put(recipeName, Math.min(map.get(recipeName), recipeCost));
-                }
-                // 사이클횟수를 초기화합니다.
-                cycleCount = 0;
+            // 이미 추가되어있는 재료라면 최소값으로 수정합니다.
+//            if (map.putIfAbsent(recipeName, recipeCost) != null) {
+//                map.put(recipeName, Math.min(map.get(recipeName), recipeCost));
+//            }
+            if (map.containsKey(recipeName)) {
+                int min = Math.min(Integer.parseInt(map.get(recipeName)), Integer.parseInt(recipeCost));
+                map.put(recipeName, String.valueOf(min));
             }
+            else {
+                map.put(recipeName, recipeCost);
+            }
+
+            // 사이클횟수를 초기화합니다.
+            cycleCount = 0;
         }
 
+        return map;
+    }
+
+    public static String calcRecipeCost(String[] material, Map<String, String> map) {
+        BigInteger recipeCost = new BigInteger("0");
+        for (String s : material) {
+            BigInteger materialCount = new BigInteger(s.replaceAll("[^0-9]", ""));
+            String materialName = s.replaceAll("[0-9]", "");
+            // 찾는 재료가 없다면 계산을 중단하고 -1을 리턴합니다.
+            if (!map.containsKey(materialName)) {
+                recipeCost = new BigInteger("-1");
+                break;
+            }
+
+            recipeCost = recipeCost.add(materialCount.multiply(new BigInteger(map.get(materialName))));
+            if (recipeCost.compareTo(new BigInteger("1000000000")) > 0) return "1000000001";
+        }
+
+        return recipeCost.toString();
     }
 }
 
@@ -72,5 +96,14 @@ public class Main {
 queue에서 제조법을 하나씩 꺼내며 물약을 제조합니다.
 물약이 제조되는 경우 cycleCount를 0으로 초기화하며 map에 새로 제도된 물약과 가격을 저장합니다.
 물약이 제조될 수 없는 경우 제조법을 다시 queue에 넣고 cycleCount를 1 증가시킵니다.
+
+문제발생 >
+A 1
+B 1
+AA=1A+1A
+BB=1B+1B
+LOVE=1AA+1BB
+BB=1A
+위와 같은 경우 LOVE를 제조한 이후 BB를 더 싸게 제조가 가능하여 LOVE를 더 싸게 제조할 수 있으나 확인하지 못하는 문제가 발생
 
  */
