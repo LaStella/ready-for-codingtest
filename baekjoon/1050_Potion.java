a0685f0c6826854173a42db0336e09eb14363f60
Jingyu Ham <jingyu.ham94@gmail.com>
2022-12-13 23:54:35 +0900

    백준 1050 물약 문제 수정
    모든 recipe를 저장하는 List를 만들었습니다.
    최소값으로 수정된 재료가 있다면 해당 재료를 포함하는 모든 recipe를 다시 큐에 넣도록 하였습니다.
    
    채점 결과 : 성공(508ms)

--- baekjoon/1050_Potion.java
+++ baekjoon/1050_Potion.java
@@ -15,79 +15,100 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        Map<String, String> map = new HashMap<>();
+        // 재료의 이름과 비용을 저장합니다.
+        Map<String, String> costMap = new HashMap<>();
         for (int i = 0 ; i < N ; i++) {
             st = new StringTokenizer(br.readLine());
-            map.put(st.nextToken(), st.nextToken());
+            costMap.put(st.nextToken(), st.nextToken());
         }
 
+        // 주어진 레시피를 저장할 리스트입니다.
+        List<String> recipeList = new ArrayList<>();
+        // 제조하려는 레시피를 저장하는 큐입니다.
         Queue<String> queue = new LinkedList<>();
+        
         for (int i = 0 ; i < M ; i++) {
-            queue.add(br.readLine());
+            String recipe = br.readLine();
+            recipeList.add(recipe);
+            queue.add(recipe);
         }
+        
+        costMap = makePotion(queue, costMap, recipeList);
 
-        map = makePotion(queue, map);
-
-        System.out.println(map.getOrDefault("LOVE", "-1"));
+        // LOVE를 제조하는 비용을 출력합니다.
+        // LOVE가 없다면 제조할 수 없으므로 -1을 출력합니다.
+        System.out.println(costMap.getOrDefault("LOVE", "-1"));
     }
 
     // 큐에 저장된 제조법에 따라 재료를 만드는 함수입니다.
-    public static Map<String, String> makePotion(Queue<String> queue, Map<String, String> map) {
+    public static Map<String, String> makePotion(Queue<String> queue, Map<String, String> costMap, List<String> recipeList) {
         int cycleCount = 0;
         // 더이상 제조법이 없거나 제조할 수 있는 물약이 없는 경우 종료합니다.
         while(!queue.isEmpty() && queue.size() != cycleCount) {
             String recipe = queue.poll();
             String[] recipeArray = recipe.split("=");
-            String recipeName = recipeArray[0];
-            String[] material = recipeArray[1].split("\\+");
+            String name = recipeArray[0];
+            String[] components = recipeArray[1].split("\\+");
 
-            String recipeCost = calcRecipeCost(material, map);
+            String cost = calcCost(components, costMap);
 
             // 제조가 불가능한 경우 제조법을 다시 큐에 넣습니다.
-            if (recipeCost.equals("-1")) {
+            if (cost.equals("-1")) {
                 cycleCount++;
                 queue.add(recipe);
                 continue;
             }
 
             // 이미 추가되어있는 재료라면 최소값으로 수정합니다.
-//            if (map.putIfAbsent(recipeName, recipeCost) != null) {
-//                map.put(recipeName, Math.min(map.get(recipeName), recipeCost));
-//            }
-            if (map.containsKey(recipeName)) {
-                int min = Math.min(Integer.parseInt(map.get(recipeName)), Integer.parseInt(recipeCost));
-                map.put(recipeName, String.valueOf(min));
-            }
-            else {
-                map.put(recipeName, recipeCost);
+            if (costMap.putIfAbsent(name, cost) != null) {
+                if (Integer.parseInt(cost) < Integer.parseInt(costMap.get(name))) {
+                    costMap.put(name, cost);
+                    // 최소값으로 수정시 레시피 중 해당 재료가 포함된 모든 레시피를 다시 큐에 넣어 비용을 계산합니다.
+                    for (String recipeWithComponent : findAllRecipeWithComponent(recipeList, name)) {
+                        queue.add(recipeWithComponent);
+                    }
+                }
             }
 
             // 사이클횟수를 초기화합니다.
             cycleCount = 0;
         }
 
-        return map;
+        return costMap;
     }
 
-    public static String calcRecipeCost(String[] material, Map<String, String> map) {
-        BigInteger recipeCost = new BigInteger("0");
-        for (String s : material) {
-            BigInteger materialCount = new BigInteger(s.replaceAll("[^0-9]", ""));
-            String materialName = s.replaceAll("[0-9]", "");
+    // name재료가 들어가는 모든 레시피를 찾아 리턴하는 함수입니다.
+    private static List<String> findAllRecipeWithComponent(List<String> recipeList, String name) {
+        List<String> list = new ArrayList<>();
+        for (String recipe : recipeList) {
+            if (recipe.contains(name)) {
+                list.add(recipe);
+            }
+        }
+        return list;
+    }
+
+    // 주어지는 구성요소들로 제조하는 비용을 계산합니다.
+    public static String calcCost(String[] components, Map<String, String> map) {
+        BigInteger cost = new BigInteger("0");
+        for (String component : components) {
+            String componentName = component.replaceAll("[0-9]", "");
             // 찾는 재료가 없다면 계산을 중단하고 -1을 리턴합니다.
-            if (!map.containsKey(materialName)) {
-                recipeCost = new BigInteger("-1");
+            if (!map.containsKey(componentName)) {
+                cost = new BigInteger("-1");
                 break;
             }
 
-            recipeCost = recipeCost.add(materialCount.multiply(new BigInteger(map.get(materialName))));
-            if (recipeCost.compareTo(new BigInteger("1000000000")) > 0) return "1000000001";
+            BigInteger componentCost = new BigInteger(map.get(componentName));
+            BigInteger componentCount = new BigInteger(component.replaceAll("[^0-9]", ""));
+
+            cost = cost.add(componentCost.multiply(componentCount));
+            if (cost.compareTo(new BigInteger("1000000000")) > 0) return "1000000001";
         }
 
-        return recipeCost.toString();
+        return cost.toString();
     }
 }
-
 
 /*
 재료를 key로 가격을 value로 가지는 map을 생성합니다.
@@ -106,4 +127,9 @@ LOVE=1AA+1BB
 BB=1A
 위와 같은 경우 LOVE를 제조한 이후 BB를 더 싸게 제조가 가능하여 LOVE를 더 싸게 제조할 수 있으나 확인하지 못하는 문제가 발생
 
+해결방법1 
+    주어지는 recipe를 map으로 저장하여 처리
+    문제발생 > 같은 이름의 제조법을 구분할 수 없는 문제가 발생
+해결방법2
+    주어지는 recipe를 List에 저장하며 costMap에 추가되는 재료가 있다면 해당 재료를 사용하는 모든 레시피를 큐에 넣습니다.    
  */
