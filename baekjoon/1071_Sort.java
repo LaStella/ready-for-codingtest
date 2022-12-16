798a91086b097d0c03c6612efcf521da189f6e72
Jingyu Ham <jingyu.ham94@gmail.com>
2022-12-16 23:21:00 +0900

    백준 1071 물약 문제 수정
    반례가 존재하므로 문제의 전체 설계를 다시합니다. (주석)
    전체 숫자를 오름차순 정렬한 뒤 앞에서부터 연속된 수를 수정합니다.
    채점 실패 이후 또다른 반례를 찾았습니다.(주석)
    
    채점 결과 : 실패

--- baekjoon/1071_Sort.java
+++ baekjoon/1071_Sort.java
@@ -13,26 +13,49 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
-        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
 
         for (int i = 0 ; i < n ; i++) {
             int number = Integer.parseInt(st.nextToken());
-            if (map.putIfAbsent(number, 1) == null) {
-                priorityQueue.add(number);
-                continue;
-            }
-            map.compute(number, (k, v)-> v+1);
+            list.add(number);
         }
 
-        
+        sortList(list);
+        printList(list);
+    }
 
+    private static void printList(List<Integer> list) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0 ; i < list.size() ; i++) {
+            int number = list.get(i);
+            sb.append(number + " ");
+        }
+        System.out.println(sb);
+    }
 
-        System.out.println();
+    private static void sortList(List<Integer> list) {
+        Collections.sort(list);
+        int prevIndex = 0;
+        for (int i = 1 ; i < list.size() ; i++) {
+            for (int j = i ; j < list.size() ; j++) {
+                if (list.get(prevIndex) + 1 != list.get(j)) {
+                    swapNumber(i, j, list);
+                    break;
+                }
+                if (j == list.size()-1) {
+                    swapNumber(prevIndex, j, list);
+                }
+            }
+            prevIndex = i;
+        }
+    }
 
+    private static void swapNumber(int index1, int index2, List<Integer> list) {
+        int number = list.get(index1);
+        list.set(index1, list.get(index2));
+        list.set(index2, number);
     }
 }
-
 
 /*
 맵에 숫자를 key로, 나온 횟수를 value로 저장합니다.
@@ -40,6 +63,17 @@ public class Main {
 우선순위큐에서 숫자가 작은것부터 차례로 뽑으며 이전 숫자와 연속하는지 확인합니다.
 연속하는 경우 다음으로 작은 숫자를 뽑아 출력하며 이전에 뽑은 숫자는 도로 큐에 넣습니다.
 연속하지 않는 경우 뽑은 숫자를 횟수만큼 출력합니다.
-우선순위큐의 크기가 2인 경우 큰 수를 먼저 출력합니다. 
+우선순위큐의 크기가 2인 경우 큰 수를 먼저 출력합니다.
 
+문제발생1 >  반례존재 1 2 3 3 4 4 5
+
+---------------------------------------------------------------------------------
+
+숫자들을 오름차순 정렬합니다.
+앞에서부터 차례로 연속된 수인지 확인합니다.
+(이전 숫자 + 1 == 현재 숫자) 인 경우 연속된 수입니다.
+연속된 수인경우 현재 숫자 다음으로 큰 수를 찾아 위치를 서로 바꿉니다.
+다음으로 큰 수를 찾지 못하는 경우 이전 숫자와 위치를 바꿉니다.
+
+문제발생2 > 반례존재 1 1 1 2 2 2 3 3 3 4 4 4
  */
