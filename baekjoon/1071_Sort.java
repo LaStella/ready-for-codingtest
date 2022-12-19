3efde8dcbb874640b7fc73970d517c9caa5cfa1d
Jingyu Ham <jingyu.ham94@gmail.com>
2022-12-19 23:45:25 +0900

    백준 1071 물약 문제 수정
    처음 생각했던 방식에서 우선순위큐에 숫자를 중복되지 않게 넣도록 변경하였습니다.
    연속하지 않는 경우 맵에서 횟수 만큼 숫자를 반복해서 출력합니다.
    연속하는 경우 다음으로 작은 숫자를 뽑아 1개만 출력합니다.
    
    채점 결과 : 실패

--- baekjoon/1071_Sort.java
+++ baekjoon/1071_Sort.java
@@ -13,66 +13,78 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        List<Integer> list = new ArrayList<>();
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        // 숫자를 key로 숫자가 나온 횟수를 value로 가지는 맵입니다.
+        Map<Integer, Integer> map = new HashMap<>();
 
+        // 맵에 숫자를 넣고 처음 들어가는 숫자는 우선순위큐에 넣습니다.
         for (int i = 0 ; i < n ; i++) {
             int number = Integer.parseInt(st.nextToken());
-            list.add(number);
+            if (map.putIfAbsent(number, 1) == null) {
+                priorityQueue.add(number);
+                continue;
+            }
+            map.compute(number, (k, v)-> v+1);
         }
 
-        sortList(list);
-        printList(list);
+        printNumber(priorityQueue, map);
     }
 
-    private static void printList(List<Integer> list) {
+    private static void printNumber(PriorityQueue<Integer> priorityQueue, Map<Integer, Integer> map) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0 ; i < list.size() ; i++) {
-            int number = list.get(i);
-            sb.append(number + " ");
-        }
-        System.out.println(sb);
-    }
+        int prevNumb = -2;
 
-    private static void sortList(List<Integer> list) {
-        Collections.sort(list);
-        int prevIndex = 0;
-        for (int i = 1 ; i < list.size()-1 ; i++) {
-            // 연속되는 경우
-            if (list.get(prevIndex) + 1 == list.get(i)) {
-                int index = getBackIndex(i, list);
-                if (index == -1) {
-                    index = getFrontIndex(prevIndex, list);
+        // 1개의 숫자만 입력된 경우
+        if (priorityQueue.size() == 1) {
+            int nowNumb = priorityQueue.poll();
+            sb.append(getRepeatNumber(nowNumb, map));
+            System.out.println(sb);
+            return;
+        }
+
+        // 큐에 남은 숫자가 2개가 될 때까지 반복합니다.
+        while (priorityQueue.size() > 2) {
+            int nowNumb = priorityQueue.poll();
+
+            // 이전 숫자와 현재 숫자가 연속된 숫자라면 다음 숫자를 뽑아 먼저 출력합니다.
+            if (prevNumb + 1 == nowNumb) {
+                int nextNumb = priorityQueue.peek();
+                sb.append(nextNumb + " ");
+                map.compute(nextNumb, (k, v)-> v-1);
+                prevNumb = nextNumb;
+                // 남은 숫자가 없는 경우 우선순위큐에서 제거합니다.
+                if (map.get(nextNumb) <= 0) {
+                    priorityQueue.poll();
                 }
-                swapNumber(i, index, list);
+                // 현재 숫자를 다시 큐에 넣습니다.
+                priorityQueue.add(nowNumb);
+                continue;
             }
-            prevIndex = i;
-        }
-    }
 
-    private static int getFrontIndex(int start, List<Integer> list) {
-        for (int i = start - 1 ; i >= 0 ; i--) {
-            if (list.get(i) != list.get(start)) {
-                return i+1;
-            }
+            // 현재 숫자를 현재 가지고 있는 만큼 반복해서 출력합니다.
+            sb.append(getRepeatNumber(nowNumb, map));
+            prevNumb = nowNumb;
         }
 
-        return 0;
-    }
+        // 남은 두 숫자가 연속된 숫자인 경우 큰 수를 먼저 출력합니다.
+        int smallNumb = priorityQueue.poll();
+        int bigNumb = priorityQueue.poll();
 
-    private static int getBackIndex(int start, List<Integer> list) {
-        for (int i = start + 1 ; i < list.size() ; i++) {
-            if (list.get(i) != list.get(start)) {
-                return i;
-            }
+        if (smallNumb + 1 == bigNumb) {
+            sb.append(getRepeatNumber(bigNumb, map));
+            sb.append(getRepeatNumber(smallNumb, map));
+            System.out.println(sb);
+            return;
         }
 
-        return -1;
+        sb.append(getRepeatNumber(smallNumb, map));
+        sb.append(getRepeatNumber(bigNumb, map));
+        System.out.println(sb.toString());
     }
 
-    private static void swapNumber(int index1, int index2, List<Integer> list) {
-        int number = list.get(index1);
-        list.set(index1, list.get(index2));
-        list.set(index2, number);
+    // 매개변수로 받은 number를 현재 미출력한 개수만큼 반복한 문자열을 리턴하는 함수입니다.
+    private static String getRepeatNumber(int number, Map<Integer, Integer> map) {
+        return (number + " ").repeat(map.get(number));
     }
 }
 
@@ -97,4 +109,14 @@ public class Main {
 문제발생2 > 반례존재 1 1 1 2 2 2 3 3 3 4 4 4
 
 (수정) 다음으로 큰 수를 찾지 못하는 경우 이전 숫자들 중 연속되지 않는 숫자를 찾아 바꿉니다.
- */
+
+---------------------------------------------------------------------------------
+
+맵에 숫자를 key로, 나온 횟수를 value로 저장합니다.
+우선순위큐에 각 숫자를 넣습니다. 같은 숫자가 여러개 존재하여도 하나만 넣습니다.
+우선순위큐에서 숫자가 작은것부터 차례로 뽑으며 이전 숫자와 연속하는지 확인합니다.
+연속하는 경우 다음으로 작은 숫자를 뽑아 출력하며 이전에 뽑은 숫자는 도로 큐에 넣습니다.
+연속하지 않는 경우 뽑은 숫자를 횟수만큼 출력합니다.
+우선순위큐에 남은 숫자가 2개라면 연속된 숫자인지 확인하여 출력 순서를 정합니다.
+
+*/
